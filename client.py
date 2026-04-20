bbe36627cff370f18810556951158115dea58dd5
Cara Ung <112128250+kiaolin@users.noreply.github.com>
2026-04-20 15:11:09 -0500

    Update client.py for better connection and command input
    Refactor client.py to improve command handling and connection setup.

--- client.py
+++ client.py
@@ -1,40 +1,23 @@
-# Imports
-from ctypes.wintypes import INT
 from getpass import getpass
 import socket
-import subprocess
 
-# Set up IP and sockets
-REMOTE_HOST = '10.0.2.5'
+REMOTE_HOST = "SERVER_IP_HERE"
 REMOTE_PORT = 4444
-client = socket.socket()
 
-# Initialize connection
-print("[-] Connection Initiating...")
+client = socket.socket()
 client.connect((REMOTE_HOST, REMOTE_PORT))
-print("[-] Connection initiated!")
 
 password = getpass()
 client.send(password.encode())
 
-# Runtime loop
 while True:
-        command = input('Enter Command: ')
-        while command == '':
-                command = input('Enter Command: ')
+    cmd = input("Enter Command: ")
 
-        command = command.encode()
+    client.send(cmd.encode())
 
-        client.send(command)
+    if cmd == "exit":
+        break
 
-        if command == b'exit':
-                break
-
-        output = client.recv(1024)
-        output = output.decode()
-        if output == 'no stdout':
-                print()
-        else:
-                print(output)
+    print(client.recv(4096).decode())
 
 client.close()
